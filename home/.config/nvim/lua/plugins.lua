3d5602e1a99caaf79295c47f503a4df410a0bb53
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-02-26 08:33:10 -0600

    feat!: Update structure of plugins

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -1,195 +1,284 @@
-return require 'packer'.startup(function(use)
+local packer = nil
 
-    -- Packer Can Manage Itself
-    use 'wbthomason/packer.nvim'
-
-    -- LSP Installer
-    use {
-        'williamboman/nvim-lsp-installer',
-        requires = 'neovim/nvim-lspconfig',
-        config = function()
-        local lspconfig = require 'lspconfig'
-        local lsp_installer = require 'nvim-lsp-installer'
-        local server_array = { 'ansiblels', 'bashls', 'clangd', 'dockerls', 'gopls', 'marksman', 'pyright', 'sumneko_lua',  'terraformls', 'texlab', 'yamlls'}
-        local lsp_installer_servers = require 'nvim-lsp-installer.servers'
-
-        -- Install Missing Language Servers
-        for _, server in ipairs(server_array) do
-            local ok, server_instance = lsp_installer_servers.get_server(server)
-            if ok then
-                if not server_instance:is_installed() then
-                    server_instance:install()
-                end
-            end
-        end
-
-        lsp_installer.on_server_ready(function(server)
-            local server_settings = {  }
-
-            if server_settings[server] then
-                server:setup(server_settings[server])
-            else
-                server:setup {}
-            end
-            vim.cmd [[do User LspAttachBuffers]]
-        end)
-
-        local opts = { noremap = true }
-        vim.api.nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        vim.api.nvim_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-        vim.api.nvim_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-        vim.api.nvim_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        vim.api.nvim_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        vim.api.nvim_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-        vim.api.nvim_set_keymap('n', '<C-f>', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+local function init()
+    if packer == nil then
+        packer = require('packer')
+        packer.init()
     end
-    }
 
-    -- Lua Engine Plugin
-    use {
+    local use = packer.use
+    packer.reset()
+
+    use('wbthomason/packer.nvim')
+
+    use({
+        'nvim-treesitter/nvim-treesitter',
+        config = function()
+            local configs = require('nvim-treesitter.configs')
+
+            configs.setup({
+                ensure_installed = {
+                    'bash',
+                    'cpp',
+                    'dockerfile',
+                    'go',
+                    'hcl',
+                    'markdown',
+                    'python',
+                    'latex',
+                    'lua',
+                    'yaml'
+                },
+                sync_install = false,
+                autotag = {
+                    enable = true,
+                },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
+    })
+    use({
+        'RRethy/nvim-base16',
+        config = function()
+            vim.cmd [[colorscheme base16-oceanicnext]]
+        end
+    })
+    use({
+        'windwp/nvim-autopairs',
+        requires = 'hrsh7th/nvim-cmp',
+        config = function()
+            local autopairs = require('nvim-autopairs')
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+
+            cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+
+            autopairs.setup({})
+        end,
+    })
+    use({
+        'lewis6991/gitsigns.nvim',
+        requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            local gitsigns = require('gitsigns')
+            gitsigns.setup({
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = 'eol',
+                    delay = 500,
+                },
+            })
+        end,
+    })
+    use({
+        'ray-x/lsp_signature.nvim',
+        config = function()
+            require('lsp_signature').setup()
+        end,
+    })
+    use({
+        'onsails/lspkind.nvim',
+        config = function()
+            local lspkind = require('lspkind')
+            lspkind.init({
+                mode = 'symbol_text',
+            })
+        end,
+    })
+    use({
+        'williamboman/mason.nvim',
+        config = function()
+            local mason = require('mason')
+
+            mason.setup()
+        end,
+    })
+    use({
+        'williamboman/mason-lspconfig.nvim',
+        requires = {
+            'williamboman/mason.nvim',
+            'neovim/nvim-lspconfig',
+        },
+        config = function()
+            local mason = require('mason')
+            mason.setup()
+
+            local lspconfig = require('lspconfig')
+
+            local mason_lspconfig = require('mason-lspconfig')
+            mason_lspconfig.setup({
+                ensure_installed = {
+                    'ansiblels',
+                    'bashls',
+                    'clangd',
+                    'dockerls',
+                    'gopls',
+                    'marksman',
+                    'pyright',
+                    'lua_ls',
+                    'terraformls',
+                    'texlab',
+                    'yamlls'
+                },
+                automatic_installation = false,
+            })
+
+            local cmp_nvim_lsp = require('cmp_nvim_lsp')
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
+            mason_lspconfig.setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end
+            })
+
+            vim.keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>')
+            vim.keymap.set('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>')
+            vim.keymap.set('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>')
+            vim.keymap.set('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>')
+            vim.keymap.set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>')
+            vim.keymap.set('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>')
+            vim.keymap.set('n', '<C-n>', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+            vim.keymap.set('n', '<C-p>', '<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+            vim.keymap.set('n', '<Leader>a', '<Cmd>lua vim.lsp.buf.format()<CR>')
+        end,
+    })
+    use({
+        'jay-babu/mason-null-ls.nvim',
+        requires = {
+            'williamboman/mason.nvim',
+            'jose-elias-alvarez/null-ls.nvim',
+        },
+        config = function()
+            local mason_null_ls = require('mason-null-ls')
+            local null_ls = require('null-ls')
+
+            mason_null_ls.setup({
+                ensure_installed = {
+                    'stylua',
+                    'black',
+                    'gitlint',
+                },
+                automatic_setup = true,
+            })
+            mason_null_ls.setup_handlers()
+
+            null_ls.setup()
+        end,
+    })
+    use({
+        'jayp0521/mason-nvim-dap.nvim',
+        requires = {
+            'williamboman/mason.nvim',
+            'mfussenegger/nvim-dap',
+        },
+        config = function()
+            local mason_nvim_dap = require('mason-nvim-dap')
+
+            mason_nvim_dap.setup({
+                ensure_installed = {
+                    'python',
+                },
+                automatic_setup = true,
+            })
+            mason_nvim_dap.setup_handlers()
+        end,
+    })
+    use({
         'hrsh7th/nvim-cmp',
         requires = {
             'L3MON4D3/LuaSnip',
             'onsails/lspkind-nvim',
             'hrsh7th/cmp-nvim-lsp',
-            'saadparwaiz1/cmp_luasnip'
-    },
+            'saadparwaiz1/cmp_luasnip',
+        },
         config = function()
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            local cmp_nvim_lsp = require 'cmp_nvim_lsp'
-            capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-
-            local luasnip = require 'luasnip'
-            local cmp = require 'cmp'
-            cmp.setup{
+            local luasnip = require('luasnip')
+            local cmp = require('cmp')
+            local lspkind = require('lspkind')
+            cmp.setup({
                 snippet = {
                     expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
+                        luasnip.lsp_expand(args.body)
+                    end,
                 },
-            mapping = {
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.close(),
-                ['<CR>'] = cmp.mapping.confirm{behavior = cmp.ConfirmBehavior.Replace, select = true},
-                ['<Tab>'] = function(fallback)
-                if vim.fn.pumvisible() == 1 then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-                elseif luasnip.expand_or_jumpable() then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-                else
-                    fallback()
-                end
-            end,
-                ['<S-Tab>'] = function(fallback)
-                if vim.fn.pumvisible() == 1 then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-                elseif luasnip.jumpable(-1) then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-                else
-                    fallback()
-                end
-            end,
-            },
-            formatting = {
-                deprecated = true,
-                format = function(_, vim_item)
-                local lspkind = require 'lspkind'
-                vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
-                return vim_item
-            end,
-            },
-            sources = {
-                {name = 'nvim_lsp'},
-                {name = 'luasnip'},
-            },
-        }
-    end
-  }
-
-    -- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-
-			configs.setup({
-				ensure_installed = {
-                    "bash",
-                    "cpp",
-                    "dockerfile",
-                    "go",
-                    "hcl",
-                    "markdown",
-                    "python",
-                    "latex",
-                    "lua",
-                    "yaml"
-				},
-				sync_install = false,
-				autotag = {
-					enable = true,
-				},
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-			})
-		end,
-	})
-
-    -- File Browsing
-    use {
+                mapping = {
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.close(),
+                    ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+                    ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+                    ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+                    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+                    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+                },
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'text_symbol',
+                        maxwidth = 80,
+                    }),
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'neorg' },
+                },
+            })
+        end,
+    })
+    use({
         'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons',
         config = function()
-        local nvim_tree = require 'nvim-tree'
+            local nvim_tree = require('nvim-tree')
 
-        nvim_tree.setup{
-            disable_netrw = true,
-            hijack_netrw = true,
-        }
+            nvim_tree.setup()
 
-        local opts = { noremap = true }
-        vim.api.nvim_set_keymap('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
-        vim.api.nvim_set_keymap('n', '<Leader>r', '<Cmd>NvimTreeRefresh<CR>', opts)
-        vim.api.nvim_set_keymap('n', '<Leader>g', '<Cmd>NvimTreeFindFile<CR>', opts)
-    end
-    }
+            vim.keymap.set('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>')
+            vim.keymap.set('n', '<Leader>r', '<Cmd>NvimTreeRefresh<CR>')
+            vim.keymap.set('n', '<Leader>g', '<Cmd>NvimTreeFindFile<CR>')
 
-    -- Quote/Bracket Auto-Completion
-    use 'jiangmiao/auto-pairs'
-
-    -- Git Declarations
-    use {
-      'lewis6991/gitsigns.nvim',
-      config = function()
-        require('gitsigns').setup()
-      end
-    }
-
-    -- Base16 Theme
-    use 'chriskempson/base16-vim'
-
-    -- Markdown Preview
-    use {
-      'iamcco/markdown-preview.nvim',
-      run = function() vim.fn['mkdp#util#install']() end,
-      ft = {'markdown'}
-    }
-
-    -- LaTeX Preview
-    use {
+            vim.cmd([[highlight NvimTreeFolderIcon guibg=blue]])
+        end,
+    })
+    use({
+        'kwkarlwang/bufresize.nvim',
+        config = function()
+            local bufresize = require('bufresize')
+            bufresize.setup()
+        end,
+    })
+    use({
+        'iamcco/markdown-preview.nvim',
+        run = function() vim.fn['mkdp#util#install']() end,
+        ft = {'markdown'}
+    })
+    use({
     'lervag/vimtex',
-    config = function ()
-        vim.g.vimtex_compiler_latexmk_engines = {
-            _ = '-xelatex'
-        }
+        config = function()
+            vim.g.vimtex_compiler_latexmk_engines = {
+                _ = '-xelatex'
+            }
+        end
+    })
+end
+
+local plugins = setmetatable({}, {
+    __index = function(_, key)
+        init()
+        return packer[key]
     end,
-    }
-  end
-)
+})
+
+return plugins
