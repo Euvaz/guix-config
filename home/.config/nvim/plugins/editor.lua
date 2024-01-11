152d0099338b6b672272239748524ed913cfdf8d
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-01-10 21:06:31 -0600

    chore(nvim): convert single-quote to double-quote

--- home/.config/nvim/plugins/editor.lua
+++ home/.config/nvim/plugins/editor.lua
@@ -5,11 +5,11 @@ return {
             {
                 "hrsh7th/nvim-cmp",
                 config = function()
-                    local autopairs = require('nvim-autopairs')
-                    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-                    local cmp = require('cmp')
+                    local autopairs = require("nvim-autopairs")
+                    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+                    local cmp = require("cmp")
 
-                    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+                    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
                     autopairs.setup({})
                 end,
