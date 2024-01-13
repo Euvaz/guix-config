3009e402a2c580fc366192a4248dc1cabdb8c376
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-01-13 17:57:06 -0600

    feat(nvim): adjust structure and add plugins

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -8,9 +8,7 @@ return {
             local autopairs = require("nvim-autopairs")
             local cmp_autopairs = require("nvim-autopairs.completion.cmp")
             local cmp = require("cmp")
-
             cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-
             autopairs.setup({})
         end,
     },
@@ -40,6 +38,69 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         opts = {}
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        cmd = "Neotree",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        init = function()
+            if vim.fn.argc(-1) == 1 then
+                local stat = vim.loop.fs_stat(vim.fn.argv(0))
+                    if stat and stat.type == "directory" then
+                    require("neo-tree")
+                end
+            end
+        end,
+        deactivate = function()
+            vim.cmd([[Neotree close]])
+        end,
+        opts = {
+            sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+            filesystem = {
+                bind_to_cwd = false,
+                follow_current_file = { enabled = true },
+                use_libuv_file_watcher = true,
+            },
+            window = {
+                mappings = {
+                    ["<space>"] = "none",
+                },
+            },
+            default_component_configs = {
+                indent = {
+                    with_expanders = true,
+                    expander_collapsed = "",
+                    expander_expanded = "",
+                    expander_highlight = "NeoTreeExpander",
+                },
+            },
+        },
+        keys = {
+            {
+                "<leader>fe",
+                function()
+                    require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+                end,
+            },
+            { "<leader>e", "<leader>fe", remap = true },
+            {
+                "<leader>ge",
+                function()
+                    require("neo-tree.command").execute({ source = "git_status", toggle = true })
+                end,
+            },
+            {
+                "<leader>be",
+                function()
+                    require("neo-tree.command").execute({ source = "buffers", toggle = true })
+                end,
+            },
+        },
     },
 }
 
