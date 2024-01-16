83a797d17c3c60fe629e16f4126ea9a83fabbcc6
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-01-16 17:44:42 -0600

    refactor(nvim): clean config

--- dot_config/nvim/lua/plugins/editor/gitsigns-nvim.lua
+++ dot_config/nvim/lua/plugins/editor/gitsigns-nvim.lua
@@ -4,17 +4,14 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-        config = function()
-            local gitsigns = require("gitsigns")
-            gitsigns.setup({
-                current_line_blame = true,
-                current_line_blame_opts = {
-                    virt_text = true,
-                    virt_text_pos = "eol",
-                    delay = 500,
-                },
-            })
-        end,
+        opts = {
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = "eol",
+                delay = 500,
+            },
+        },
     },
 }
 
--- dot_config/nvim/lua/plugins/editor/toggleterm-nvim.lua
+++ dot_config/nvim/lua/plugins/editor/toggleterm-nvim.lua
@@ -1,21 +1,17 @@
 return {
     {
         "akinsho/toggleterm.nvim",
-        version = "*",
-        config = function()
-            local toggleterm = require("toggleterm")
-            toggleterm.setup({
-                open_mapping = [[<C-\>]],
-                close_on_exit = true,
-                direction = "float",
-                float_opts = {
-                    border = "curved",
-                    width  = 160,
-                    height = 30,
-                    winblend = 10,
-                },
-            })
-        end,
+        opts = {
+            open_mapping = [[<C-\>]],
+            close_on_exit = true,
+            direction = "float",
+            float_opts = {
+                border = "curved",
+                width  = 160,
+                height = 30,
+                winblend = 10,
+            },
+        },
     },
 }
 
