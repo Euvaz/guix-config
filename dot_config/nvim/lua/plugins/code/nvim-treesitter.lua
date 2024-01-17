f782ebd8970aec7b00a70c500fc09b600d39c339
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-01-16 22:00:37 -0600

    feat(nvim): add treesitter

---
+++ dot_config/nvim/lua/plugins/code/nvim-treesitter.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local nvim_treesitter = require("nvim-treesitter.configs")
+            nvim_treesitter.setup({
+                ensure_installed = {
+                    "bash",
+                    "css",
+                    "dockerfile",
+                    "gitignore",
+                    "go",
+                    "hcl",
+                    "html",
+                    "json",
+                    "latex",
+                    "lua",
+                    "markdown",
+                    "python",
+                    "scss",
+                    "templ",
+                    "toml",
+                    "vim",
+                    "xml",
+                    "yaml",
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+}
