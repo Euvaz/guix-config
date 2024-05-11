1cb9c07b14253b515e46c801ce39db33728a186d
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-05-10 19:11:15 -0500

    feat(wezterm): add initial config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require "wezterm"
+local config = {}
+
+-- Disable Top Bar
+config.enable_tab_bar = false
+
+-- Color Scheme
+config.color_scheme = "nord"
+
+-- Cursor Style
+config.default_cursor_style = "SteadyBar"
+
+return config
