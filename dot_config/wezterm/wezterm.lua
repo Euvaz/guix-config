68d36b248da726375ebd951bd436dc693d85a1a4
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-05-10 21:39:49 -0500

    feat(wezterm): specify Catppuccin colorscheme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = {}
 config.enable_tab_bar = false
 
 -- Color Scheme
-config.color_scheme = "nord"
+config.color_scheme = "Catppuccin Mocha"
 
 -- Cursor Style
 config.default_cursor_style = "SteadyBar"
