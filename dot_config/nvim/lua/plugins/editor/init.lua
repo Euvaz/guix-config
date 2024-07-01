90cb178ff766b92a6d703e048972f3ba82835f44
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-07-01 15:34:31 -0500

    feat(nvim): replace neo-tree.nvim with oil.nvim

--- dot_config/nvim/lua/plugins/editor/init.lua
+++ dot_config/nvim/lua/plugins/editor/init.lua
@@ -1,11 +1,11 @@
 local plugins_group = "plugins.editor."
 
 return {
-  -- TODO: Enable telescope.nvim
   require(plugins_group .. "barbar-nvim"),
   require(plugins_group .. "gitsigns-nvim"),
   require(plugins_group .. "indent-blankline-nvim"),
-  require(plugins_group .. "neo-tree-nvim"),
+  require(plugins_group .. "oil-nvim"),
+  require(plugins_group .. "telescope-nvim"),
   require(plugins_group .. "toggleterm-nvim"),
 }
 
