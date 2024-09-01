9c7ac053a05a2ac9f71623581c0f946a43b8f06f
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-08-31 21:24:53 -0500

    feat(nvim): lazy load gitsigns.nvim

--- dot_config/nvim/lua/plugins/editor/gitsigns-nvim.lua
+++ dot_config/nvim/lua/plugins/editor/gitsigns-nvim.lua
@@ -1,6 +1,7 @@
 ---@type LazySpec
 return {
   "lewis6991/gitsigns.nvim",
+  event = { "BufNewFile", "BufReadPost", "BufWritePost "},
   opts = {
     current_line_blame = true,
     current_line_blame_opts = {
