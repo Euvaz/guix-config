74deb82ed111b8ea93553e3d1e984276a39f1c74
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-09-20 00:28:58 -0500

    feat(wezterm): update font

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,6 +4,9 @@ local config = {}
 -- Disable Top Bar
 config.enable_tab_bar = false
 
+-- Font
+config.font = wezterm.font "FiraCode Nerd Font Mono"
+
 -- Color Scheme
 config.color_scheme = "Catppuccin Mocha"
 
