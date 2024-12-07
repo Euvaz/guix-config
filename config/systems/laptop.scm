31961b2479ef1caacd62453dd371bc6cd2b72c74
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-12-07 00:13:38 -0600

    chore(guix): enforce convention

--- config/systems/laptop.scm
+++ config/systems/laptop.scm
@@ -1,8 +1,10 @@
 (define-module (config systems laptop)
                #:use-module (gnu)
+               ; #:use-module (gnu home)
                #:use-module (gnu packages firmware)
+               #:use-module (gnu packages terminals)
                #:use-module (gnu services desktop)
-               #:use-module (gnu services guix)
+               #:use-module (gnu services guix) ; B
                #:use-module (gnu services virtualization)
 
                #:use-module (nongnu packages linux)
@@ -17,28 +19,27 @@
   (operating-system
     (inherit %guix-base)
     (host-name "laptop.local")
-  
+
     ;; Linux kernel and initrd
     (kernel linux)
     (initrd microcode-initrd)
-  
+
     ;; Additional firmware
     (firmware (cons* iwlwifi-firmware
                      %base-firmware))
-  
+
     ;; Bootloader with EFI
     (bootloader (bootloader-configuration
                   (bootloader grub-efi-bootloader)
-                  (targets (list "/boot/efi"))
-  	        (terminal-outputs '(console))))
-  
+                  (targets (list "/boot/efi"))))
+
     ;; Mapped devices for encrypted root partition
     (mapped-devices (list (mapped-device
                             (source (uuid
                                      "acadaf63-afd1-4a00-95c6-2074661ad159"))
                             (target "cryptroot")
                             (type luks-device-mapping))))
-  
+
     ;; File systems with encryption
     (file-systems (cons* (file-system
                            (mount-point "/")
@@ -50,23 +51,22 @@
                            (device (uuid "06B2-C813"
                                          'fat32))
                            (type "vfat"))
-               	%base-file-systems))
-  
+                         %base-file-systems))
+
     ;; User account, base user accounts, and groups
     (users (cons* (user-account
                     (name %user-name)
                     (comment "Virgil")
                     (group "users")
-                    (home-directory (string-append "/home/" %user-name))
                     (supplementary-groups '("wheel" "netdev" "audio" "video" "libvirt")))
                   %base-user-accounts))
-  
+
     ;; OVMF and base packages
     (packages
       (append
         (list ovmf-x86-64)
         %guix-base-packages))
-  
+
     ;; Desktop environment, virtualization, and base services
     (services
       (append
@@ -76,9 +76,9 @@
               (service libvirt-service-type)
               (service virtlog-service-type)
               (extra-special-file "/usr/share/OVMF/OVMF_CODE.fd"
-                (file-append ovmf "/share/firmware/ovmf_code_x64.bin"))
+                                  (file-append ovmf "/share/firmware/ovmf_code_x64.bin"))
               (extra-special-file "/usr/share/OVMF/OVMF_VARS.fd"
-                (file-append ovmf "/share/firmware/ovmf_vars_x64.bin")))
+                                  (file-append ovmf "/share/firmware/ovmf_vars_x64.bin")))
         %desktop-services))))
 
 ;; Instantiate Guix
