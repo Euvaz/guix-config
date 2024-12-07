165410083ca335ad33c9088919ac5f09036f75e7
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2024-12-07 02:09:06 -0600

    feat(guix): enable audio via PipeWire

--- config/home/guix-home.scm
+++ config/home/guix-home.scm
@@ -1,8 +1,10 @@
 (define-module (config home guix-home)
                #:use-module (gnu)
                #:use-module (gnu home)
+               #:use-module (gnu home services desktop)
                #:use-module (gnu home services dotfiles)
                #:use-module (gnu home services shells)
+               #:use-module (gnu home services sound)
                #:use-module (gnu packages terminals)
 
                #:export (%guix-home))
@@ -16,7 +18,9 @@
       (list (service home-dotfiles-service-type
                      (home-dotfiles-configuration
                        (directories '("../../files/"))))
-            (service home-bash-service-type)))))
+            (service home-bash-service-type)
+            (service home-pipewire-service-type)
+            (service home-dbus-service-type)))))
 
 ;; Instantiate Guix Home
 %guix-home
