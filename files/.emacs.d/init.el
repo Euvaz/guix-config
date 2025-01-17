0f8d3d62548d45aaf3d574bf554023a9bc18539f
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-01-16 19:07:00 -0600

    feat(emacs): add base config

---
+++ files/.emacs.d/init.el
@@ -0,0 +1,16 @@
+;; Disable UI elements
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1))
+
+;; Display line numbers
+(setq display-line-numbers-type 'relative)
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+;; Load theme and define font
+(load-theme 'wombat)
+(set-face-attribute 'default nil :font "Fira Code" :height 160))
+
+;; Install and configure Magit
+(use-package magit
+  :ensure t)
