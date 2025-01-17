edd83fe9200fb2df9d3bc763287b40861f7ad950
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-01-16 22:08:26 -0600

    feat(emacs): define indentation rules and package archives

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -3,6 +3,9 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; Define indentation rules
+(setq indent-tabs-mode nil)
+
 ;; Display line numbers
 (setq display-line-numbers-type 'relative)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
@@ -10,6 +13,15 @@
 ;; Load theme and define font
 (load-theme 'wombat)
 (set-face-attribute 'default nil :font "Fira Code" :height 160)
+
+;; Initialize package archives
+(require 'package)
+(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize
+  (unless package-archive-contents
+    (package-refresh-contents)))
 
 ;; Install and configure Magit
 (use-package magit
