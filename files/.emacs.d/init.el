21c7775ea43e4c566945cea142944e78bbbe4489
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-01-16 22:42:43 -0600

    refactor(emacs): disable package refreshing

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -18,10 +18,7 @@
 (require 'package)
 (setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
-
-(package-initialize
-  (unless package-archive-contents
-    (package-refresh-contents)))
+(package-initialize)
 
 ;; Install and configure Magit
 (use-package magit
