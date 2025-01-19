8c8fc089fbd4c8dcbf100930fb898c2e1520ec7e
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-01-18 18:50:42 -0600

    feat(emacs): establish lsp-mode and tree-sitter base

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -3,6 +3,10 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; Utilize separate custom-set-variables file
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
+
 ;; Define indentation rules
 (setq indent-tabs-mode nil)
 
@@ -23,3 +27,24 @@
 ;; Install and configure Magit
 (use-package magit
   :ensure t)
+
+;; Install and configure lsp-mode
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred))
+
+(use-package treesit-auto
+  :ensure t
+  :config
+  (setq treesit-auto-langs '(bash commonlisp elisp go go-mod python yaml))
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
+(setq treesit-language-source-alist
+      '((bash . ("https://github.com/tree-sitter/tree-sitter-bash" "master" "src"))
+        (commonlisp ("https://github.com/theHamsta/tree-sitter-commonlisp" "master" "src"))
+        (elisp . ("https://github.com/Wilfred/tree-sitter-elisp" "main" "src"))
+        (go . ("https://github.com/tree-sitter/tree-sitter-go" "master" "src"))
+        (go-mod . ("https://github.com/camdencheek/tree-sitter-go-mod" "main" "src"))
+        (python . ("https://github.com/tree-sitter/tree-sitter-python" "master" "src"))
+        (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "master" "src"))))
