491d49606cdb8e03fca717c616aa5329091cff24
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-02-23 16:06:10 -0600

    refactor(nvim): prune unused plugins

--- files/.config/nvim/lua/plugins/editor/init.lua
+++ files/.config/nvim/lua/plugins/editor/init.lua
@@ -1,8 +1,6 @@
 local plugins_group = "plugins.editor."
 
 return {
-  require(plugins_group .. "barbar-nvim"),
-  require(plugins_group .. "gitsigns-nvim"),
   require(plugins_group .. "indent-blankline-nvim"),
   require(plugins_group .. "oil-nvim"),
   require(plugins_group .. "telescope-nvim")
