04fe4f3da0841adf90b344b003b4351d9f46dbbe
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-02-23 23:26:40 -0600

    fix(nvim): update to Unicode symbols

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -28,6 +28,23 @@ lazy.setup({
                 "zipPlugin"
             }
         }
+    },
+    ui = {
+        icons = {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        }
     }
 })
 
