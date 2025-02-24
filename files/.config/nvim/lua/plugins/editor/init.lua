3cea099c76d488184e0b06a6e6c04809d8d9f032
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-02-23 23:56:33 -0600

    refactor(nvim): prune oil.nvim

--- files/.config/nvim/lua/plugins/editor/init.lua
+++ files/.config/nvim/lua/plugins/editor/init.lua
@@ -2,6 +2,5 @@ local plugins_group = "plugins.editor."
 
 return {
     require(plugins_group .. "indent-blankline-nvim"),
-    require(plugins_group .. "oil-nvim"),
     require(plugins_group .. "telescope-nvim")
 }
