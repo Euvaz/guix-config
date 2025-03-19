7382e6ad390f28bace98acf1be8a18de9cc511f6
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-03-19 13:52:31 -0500

    feat(emacs): add autocomplete

--- files/.config/emacs/init.el
+++ files/.config/emacs/init.el
@@ -7,6 +7,9 @@
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
 
+;; Wayland optimization
+(setq-default pgtk-wait-for-event-timeout 0)
+
 ;; Disable backup files
 (setq make-backup-files nil)
 
@@ -32,15 +35,53 @@
 (package-initialize)
 
 ;; Package configuration
-(use-package magit
+(use-package vertico
   :ensure t
-  :defer t
-  :commands (magit-status magit))
+  :init
+  (setq enable-recursive-minibuffers t)
+  :config
+  (vertico-mode t))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package corfu
+  :ensure t
+  :init
+  (global-corfu-mode)
+  (corfu-history-mode)
+  :config
+  (setq corfu-auto t
+        corfu-cycle t
+        corfu-on-exact-match nil
+        corfu-preselect 'prompt)
+  :bind (:map corfu-map
+              ("TAB" . corfu-next)
+              ([tab] . corfu-next)
+              ("S-TAB" . corfu-previous)
+              ([backtab] . corfu-previous)))
 
 (use-package lsp-mode
   :ensure t
   :defer t
-  :commands (lsp lsp-deferred))
+  :commands (lsp lsp-deferred)
+  :config
+  (setq gc-cons-threshold 100000000
+        read-process-output-max (* 1024 1024)
+        lsp-completion-provider :none)
+  (defun corfu-lsp-setup ()
+    (setq-local completion-styles '(orderless)
+                completion-category-defaults nil))
+  (add-hook 'lsp-completion-mode-hook #'corfu-lsp-setup))
+
+(use-package magit
+  :ensure t
+  :defer t
+  :commands (magit-status magit))
 
 (use-package zig-mode
   :ensure t
