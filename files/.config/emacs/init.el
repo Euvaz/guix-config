aa8b22d9c9531968c737221705e311740cead337
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-04-02 05:23:25 -0500

    feat(emacs): trim treesit-langauge-source-alist and add langs

--- files/.config/emacs/init.el
+++ files/.config/emacs/init.el
@@ -19,12 +19,19 @@
 
 ;; Tree-sitter language sources
 (setq treesit-language-source-alist
-      '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
-        (css . ("https://github.com/tree-sitter/tree-sitter-css"))
-        (go . ("https://github.com/tree-sitter/tree-sitter-go"))
-        (gomod . ("https://github.com/camdencheek/tree-sitter-go-mod" "main" "src"))
-        (html . ("https://github.com/tree-sitter/tree-sitter-html"))
-        (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-        (yaml . ("https://github.com/ikatyang/tree-sitter-yaml"))))
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (go "https://github.com/tree-sitter/tree-sitter-go")
+        (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (templ "https://github.com/vrischmann/tree-sitter-templ")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+        (zig "https://github.com/tree-sitter-grammars/tree-sitter-zig")))
 
 ;;; init.el ends here
