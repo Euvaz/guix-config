dcd8c9cd1d7d9ce154d0ca0ffc6cc40a10698d91
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-04-17 17:35:20 -0500

    feat(sway): add and update packages

--- config/home/services/desktop.scm
+++ config/home/services/desktop.scm
@@ -1,25 +1,32 @@
 (define-module (config home services desktop)
   #:use-module (gnu)
   #:use-module (gnu home services)
-  #:use-module (gnu packages admin)
-  #:use-module (gnu packages curl)
-  #:use-module (gnu packages emacs)
-  #:use-module (gnu packages emacs-xyz)
-  #:use-module (gnu packages fonts)
-  #:use-module (gnu packages gnupg)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages package-management)
-  #:use-module (gnu packages pdf)
-  #:use-module (gnu packages pulseaudio)
-  #:use-module (gnu packages shellutils)
-  #:use-module (gnu packages shells)
-  #:use-module (gnu packages ssh)
-  #:use-module (gnu packages terminals)
-  #:use-module (gnu packages tls)
-  #:use-module (gnu packages version-control)
-  #:use-module (gnu packages wm)
-  #:use-module (gnu packages zig)
-  #:use-module (gnu packages zig-xyz)
+  #:use-module (gnu packages admin) ; btop
+  #:use-module (gnu packages compression) ; unzip
+  #:use-module (gnu packages curl) ; curl
+  #:use-module (gnu packages emacs) ; emacs-next-pgtk
+  #:use-module (gnu packages emacs-xyz) ; emacs-org-texlive-collection
+  #:use-module (gnu packages fonts) ; font-awesome, font-fire-code, font-google-noto-emoji, font-victor-mono
+  #:use-module (gnu packages gnupg) ; pinentry
+  #:use-module (gnu packages linux) ; brightnessctl, ebtables, numactl
+  #:use-module (gnu packages node) ; node
+  #:use-module (gnu packages package-management) ; flatpak
+  #:use-module (gnu packages pdf) ; zathura, zathura-pdf-mupdf
+  #:use-module (gnu packages pulseaudio) ; pavucontrol
+  #:use-module (gnu packages python) ; python
+  #:use-module (gnu packages python-xyz) ; python-lsp-server
+  #:use-module (gnu packages rust-apps) ; eza, ripgrep
+  #:use-module (gnu packages shells) ; zsh
+  #:use-module (gnu packages shellutils) ; direnv
+  #:use-module (gnu packages ssh) ; openssh
+  #:use-module (gnu packages statistics) ; r
+  #:use-module (gnu packages terminals) ; foot
+  #:use-module (gnu packages tls) ; openssl
+  #:use-module (gnu packages tor-browsers) ; mullvadbrowser
+  #:use-module (gnu packages version-control) ; git
+  #:use-module (gnu packages virtualization) ; qemu, virt-manager
+  #:use-module (gnu packages wm) ; sway, swaybg, swayidle, swaylock, wmenu
+  #:use-module (gnu packages xdisorg) ; wl-clipboaard
 
   #:export (home-desktop-service-type))
 
@@ -31,25 +38,36 @@
         ebtables
         emacs-next-pgtk
         emacs-org-texlive-collection
+        eza
         flatpak
+        font-awesome
         font-fira-code
         font-google-noto-emoji
+        font-victor-mono
         foot
         git
+        mullvadbrowser
+        node
         numactl
         openssh
         openssl
         pavucontrol
         pinentry
+        python
+        python-lsp-server
+        qemu
+        r
+        ripgrep
         sway
         swaybg
         swayidle
         swaylock
+        unzip
+        virt-manager
+        wl-clipboard
         wmenu
         zathura
         zathura-pdf-mupdf
-        zig
-        zig-zls
         zsh))
 
 (define home-desktop-service-type
