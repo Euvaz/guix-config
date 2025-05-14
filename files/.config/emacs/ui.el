ce127fec2b57b38a0054c3660f0526925ce14cf7
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-05-14 08:06:25 -0500

    feat(emacs): reorder config and define transparency

--- files/.config/emacs/ui.el
+++ files/.config/emacs/ui.el
@@ -1,21 +1,6 @@
 ;;; ui.el -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;; UI tweaks.
-;;; Code:
-
-;; Disable UI elements
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-;; Theme options
-(setq modus-themes-italic-constructs t)
-
-;; Load theme and define font
-(load-theme 'modus-vivendi-tinted t)
-(set-face-attribute 'default nil :font "Fira Code" :height 160)
-(set-face-attribute 'italic nil :font "Victor Mono" :height 160)
-
 ;;; Variables:
 
 ;; Adjust startup behavior
@@ -23,7 +8,25 @@
       initial-major-mode 'emacs-lisp-mode
       inhibit-startup-screen t)
 
+;; Theme options
+(setq modus-themes-italic-constructs t)
+
 ;; Wayland optimization
 (setq-default pgtk-wait-for-event-timeout 0)
+
+;;; Code:
+
+;; Disable UI elements
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Load theme and define font
+(load-theme 'modus-vivendi-tinted t)
+(set-face-attribute 'default nil :font "Fira Code" :height 160)
+(set-face-attribute 'italic nil :font "Victor Mono" :height 160)
+
+;; Define frame's background transparency
+(set-frame-parameter nil 'alpha-background 97)
 
 ;;; ui.el ends here
