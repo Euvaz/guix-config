7733fec13f9bd6188ef2ca3d3837e591227bdd56
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-09-20 11:33:45 -0500

    feat(guix): update channels

--- config/systems/guix-channels.scm
+++ config/systems/guix-channels.scm
@@ -3,16 +3,6 @@
 
   #:export (%guix-channels))
 
-(define %guix-channel
-  (channel
-   (name 'guix)
-   (url "https://git.savannah.gnu.org/git/guix.git")
-   (introduction
-    (make-channel-introduction
-     "9edb3f66fd807b096b48283debdcddccfea34bad"
-     (openpgp-fingerprint
-      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
-
 (define %nonguix-channel
   (channel
    (name 'nonguix)
@@ -24,6 +14,5 @@
       "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
 
 (define %guix-channels
-  (append (list %guix-channel
-                %nonguix-channel)
-          %default-channels))
+  (cons* %nonguix-channel
+         %default-channels))
