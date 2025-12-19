71ea9957250e6626d8daba1b11e34443db687aa5
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2025-12-19 02:37:21 -0600

    refactor(emacs): configure use-package to always ensure

--- files/.config/emacs/packages.el
+++ files/.config/emacs/packages.el
@@ -11,24 +11,22 @@
 ;; Bootstrap use-package
 (eval-when-compile (require 'use-package))
 (setq use-package-verbose t
-      use-package-compute-statistics t)
+      use-package-compute-statistics t
+      use-package-always-ensure t)
 
 (use-package vertico
-  :ensure t
   :init
   (setq enable-recursive-minibuffers t)
   :config
   (vertico-mode t))
 
 (use-package orderless
-  :ensure t
   :config
   (setq completion-styles '(orderless basic)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package corfu
-  :ensure t
   :init
   (global-corfu-mode)
   (corfu-history-mode)
@@ -45,16 +43,17 @@
               ([backtab] . #'corfu-previous)))
 
 (use-package smartparens
-  :ensure t
   :hook (prog-mode text-mode))
 
 (use-package org
+  :ensure nil
   :config
   (setq org-todo-keywords '((sequence "TODO(t)" "PROG(p)" "|" "DONE(d)")
                             (sequence "BACKLOG(b)" "|" "CANCELLED(c@)")))
   :mode ("\\.org\\'" . org-mode))
 
 (use-package ob
+  :ensure nil
   :after org
   :config
   (setq org-confirm-babel-evaluate nil
@@ -69,7 +68,6 @@
      )))
 
 (use-package emms
-  :ensure t
   :config
   (setq emms-player-list '(emms-player-mpv))
   (emms-all)
@@ -77,31 +75,27 @@
   :commands (emms))
 
 (use-package magit
-  :ensure t
   :commands (magit-status magit))
 
 (use-package magit-todos
-  :ensure t
   :after magit
   :config (magit-todos-mode 1))
 
 (use-package geiser
-  :ensure t
   :commands (geiser run-geiser)
   :config (setq geiser-mode-start-repl-p t
                 geiser-repl-query-on-kill-p nil))
 
 (use-package geiser-guile
-  :ensure t
   :commands (geiser-guile run-guile))
 
 (use-package sly
-  :ensure t
   :commands (sly sly-connect)
   :config (setq sly-lisp-implementations
                 '((sbcl ("sbcl") :coding-system utf-8-unix))))
 
 (use-package treesit
+  :ensure nil
   :defer t
   :init
   (setq treesit-language-source-alist
@@ -120,50 +114,61 @@
           )))
 
 (use-package bash-ts-mode
+  :ensure nil
   :init
   (add-to-list 'major-mode-remap-alist '(sh-mode . bash-ts-mode))
   :mode ("\\.sh\\'" . bash-ts-mode))
 
 (use-package c-ts-mode
+  :ensure nil
   :init
   (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
   :mode ("\\.c\\'" . c-ts-mode))
 
 (use-package cmake-ts-mode
+  :ensure nil
   :mode ("\\.cmake\\'" . cmake-ts-mode))
 
 (use-package css-ts-mode
+  :ensure nil
   :init
   (add-to-list 'major-mode-remap-alist '(css-mode . css-ts-mode))
   :mode ("\\.css\\'" . css-ts-mode))
 
 (use-package go-ts-mode
+  :ensure nil
   :mode ("\\.go\\'" . go-ts-mode))
 
 (use-package go-mod-ts-mode
+  :ensure nil
   :mode ("go\\.mod\\'" . go-mod-ts-mode))
 
 (use-package html-ts-mode
+  :ensure nil
   :config
   (add-to-list 'major-mode-remap-alist '(mhtml-mode . html-ts-mode))
   :mode ("\\.html\\'" . html-ts-mode))
 
 (use-package json-ts-mode
+  :ensure nil
   :config
   (add-to-list 'major-mode-remap-alist '(js-json-mode . json-ts-mode))
   :mode ("\\.json\\'" . json-ts-mode))
 
 (use-package python-ts-mode
+  :ensure nil
   :config
   (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
   :mode ("\\.py[iw]?\\'" . python-ts-mode))
 
 (use-package toml-ts-mode
+  :ensure nil
   :config
   (add-to-list 'major-mode-remap-alist '(conf-toml-mode . toml-ts-mode))
   :mode ("\\.toml\\'" . toml-ts-mode))
 
 (use-package yaml-ts-mode
+  :ensure nil
   :mode ("\\.ya?ml\\'" . yaml-ts-mode))
 
 ;;; packages.el ends here
