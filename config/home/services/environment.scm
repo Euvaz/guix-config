dfb54f7c82108a1ec0eae914d7181a4521429913
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2026-03-03 22:10:15 -0600

    feat(guix): define theme and icon env vars

--- config/home/services/environment.scm
+++ config/home/services/environment.scm
@@ -6,7 +6,12 @@
 
 (define (home-environment-variables-configuration-gexp config)
   '(("EDITOR" . "emacs -nw")
+    ("GTK_ICON_THEME" . "Qogir-Dark")
+    ("GTK_THEME" . "Flat-Remix-GTK-Blue-Dark")
     ("PATH" . "${PATH}")
+    ("QT_QPA_PLATFORM" . "wayland")
+    ("QT_QPA_PLATFORMTHEME" . "gtk3")
+    ; ("QT_STYLE_OVERRIDE" . "Flat-Remix-GTK-Blue-Dark")
     ("VISUAL" . "emacs -nw")))
 
 (define home-environment-variables-configuration-service-type
